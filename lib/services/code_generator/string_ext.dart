dc9a4942fdd94eb3aa410ee88098788b4ab44949
mikeborodin-af <mykhailo.borodin@appsfactory.de>
2023-08-18 15:44:46 +0200

    [update] logic and readme

--- lib/services/code_generator/string_ext.dart
+++ lib/services/code_generator/string_ext.dart
@@ -1,14 +1,11 @@
-extension Ext on String {
-  String capitalize() {
-    return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
-  }
+import 'package:figma_vars_to_dart/services/code_generator/dart_keywords.dart';
 
-  String toCamelCase() {
+extension Ext on String {
+  String camelCase() {
     if (isEmpty) {
       return '';
     }
-
-    List<String> words = trim().split(RegExp(r'[ /-]'));
+    List<String> words = trim().splitByMultipleSeparators();
     String camelCase = words[0].toLowerCase();
 
     for (int i = 1; i < words.length; i++) {
@@ -16,25 +13,40 @@ extension Ext on String {
       camelCase +=
           word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
     }
-
     return camelCase;
   }
 
-  String sanitizeVariableName() {
-    final reserved = {'return', 'final', 'late', 'if'};
+  String pascalCase() {
+    return camelCase().capitalizeFirstLetter();
+  }
+
+  String escapeKeywords() {
     String result = replaceAll(RegExp(r'[^\w\s]'), '');
 
-    if (reserved.contains(result)) {
+    if (dartKeywords.contains(result)) {
       result = '${result}_';
     }
     return result;
   }
-
+  String escapeSingleAndDoubleQuotes() {
+    return replaceAll("'", "\\'").replaceAll('"', '\\"');
+  }
   String capitalizeFirstLetter() {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
+}
 
-  String escapeSingleAndDoubleQuotes() {
-  return replaceAll("'", "\\'").replaceAll('"', '\\"');
+extension _Helpers on String {
+  List<String> splitByMultipleSeparators() {
+    String pattern = [
+      ' ',
+      '/',
+      '_',
+      '-',
+    ].map((sep) => "\\$sep").join("|");
+    final regex = RegExp(pattern);
+    return split(regex);
   }
+
+
 }
---
+++ test/services/code_generator/string_ext_test.dart
@@ -0,0 +1,28 @@
+import 'package:figma_vars_to_dart/services/code_generator/string_ext.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('should format varibale name as camelCase', () {
+    var expected = 'colorBrand';
+
+    expect('color-brand'.camelCase(), expected);
+    expect('color brand'.camelCase(), expected);
+    expect('color/brand'.camelCase(), expected);
+    expect('color_brand'.camelCase(), expected);
+  });
+  test('should format varibale name as PamelCase', () {
+    var expected = 'ColorBrand';
+
+    expect('color-brand'.pascalCase(), expected);
+    expect('color brand'.pascalCase(), expected);
+    expect('color/brand'.pascalCase(), expected);
+    expect('color_brand'.pascalCase(), expected);
+  });
+
+
+  test('should escape Dart keywords', (){
+    expect('await'.escapeKeywords(), 'await_');
+    expect('return'.escapeKeywords(), 'return_');
+    expect('class'.escapeKeywords(), 'class_');
+  });
+}
