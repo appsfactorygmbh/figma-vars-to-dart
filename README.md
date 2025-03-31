dbdf7bdc867960c62440602d3e778251ff4ad520
af-sashatalalaieva <oleksandra.talalaieva@appsfactory.de>
2025-03-31 11:51:29 +0200

    [ADD] Download image assets  (#24)

--- README.md
+++ README.md
@@ -7,42 +7,38 @@ Bring your Figma design system and Flutter app implementation closer together by
 * There's manual interaction with Figma UI required and it takes time on bigger projects
 * No easy way to detect which variables changed/updated without manual inspection
 
-
-Use cases:
-* Light / dark mode
-* Adaptive layouts
-
 ## Installation
 ```yaml
 dev_dependencies:
-  figma_vars_to_dart: ^0.0.4 
+  figma_vars_to_dart: ^0.1.0 
 ```
 or 
 
 ```bash
 flutter pub add figma_vars_to_dart --dev
 ```
+## Commands
+- [Figma Variables](#figma-variables)
+- [Downloading Images](#downloading-images)
 
 ## Usage
-
-### Prepare your Figma
-
-Make sure your Figma project uses variables. 
-
-<details open>
-  <summary>Sample inputs for the demo purposes</summary>
-
-  ![Input 1](https://raw.githubusercontent.com/appsfactorygmbh/figma-vars-to-dart/main/images/figma_collection_1.png)
-  ![Input 2](https://raw.githubusercontent.com/appsfactorygmbh/figma-vars-to-dart/main/images/figma_collection_2.png)
-</details>
-
 
 ### Create the token
 
 In order to interact with Figma API you will need a personal access token. 
 To create this token you can follow [this](https://help.figma.com/hc/en-us/articles/8085703771159-Manage-personal-access-token) figma tutorial.
 
-### Run the command
+### Figma Variables
+Make sure your Figma project uses variables. 
+
+<details>
+  <summary>Sample inputs for the demo purposes</summary>
+
+  ![Input 1](https://raw.githubusercontent.com/appsfactorygmbh/figma-vars-to-dart/main/images/figma_collection_1.png)
+  ![Input 2](https://raw.githubusercontent.com/appsfactorygmbh/figma-vars-to-dart/main/images/figma_collection_2.png)
+</details>
+
+#### Run the command
 
 ```bash
 dart pub run figma_vars_to_dart generate \
@@ -52,9 +48,9 @@ dart pub run figma_vars_to_dart generate \
 	 --jsonOutputFile vars.json 
 ```
 
-### Use the code
+#### Use the code
 
-<details open>
+<details>
   <summary>Generated files</summary>
 
 `color_primitives.dart`
@@ -124,8 +120,37 @@ export 'color_semantics.dart';
 
 You can use any state management you prefer to provide these values to your widgets.
 
-## Example
+
+#### Example
 See the [example](https://github.com/appsfactorygmbh/figma-vars-to-dart/tree/main/example) for more information.
+
+### Downloading Images
+To use the download-images feature, create sections for specific use cases. Each section name must follow the format "APP_ASSET_$section", where $section represents a specific use case. Then, add the images you want to download and convert them into components.
+<details>
+  <summary>Sample for the demo purposes</summary>
+
+  ![Input 3](https://raw.githubusercontent.com/appsfactorygmbh/figma-vars-to-dart/main/images/figma_section.png)
+</details>
+
+#### Run the command
+
+```bash
+dart pub run figma_vars_to_dart download-images \
+	 --token $FIGMA_TOKEN \
+	 --fileId $YOUR_FIGMA_FILE_ID 
+
+```
+#### Arguments 
+| Argument         | Type    | Required | Default       | Description |
+|------------------|---------|----------|---------------|-------------|
+| `--fileId`       | String  | ✅ Yes   | None          | The **Figma file ID**, which can be found in the Figma file URL. |
+| `--token`        | String  | ✅ Yes   | None          | Your **Figma personal access token** for authentication. |
+| `--outputFolder` | String  | ❌ No    | `assets`      | The folder where downloaded images will be stored. Defaults to `assets/`. |
+| `--format`       | String  | ❌ No    | `png`         | The format of the downloaded images. Allowed values: `jpg`, `png`, `svg`. |
+| `--scale`        | String  | ❌ No    | `1.0,2.0,3.0` | The scales for downloaded images (comma-separated). Must be between **0.01 and 4.0**. **Note:** If the format is `svg`, this argument is ignored and set to `1.0`. |
+| `--section`      | String  | ❌ No    | `APP_ASSET_`        | Specify **Figma sections** to download (comma-separated). By default, all `APP_ASSET_$section` sections are considered. |
+| `--force`        | Flag    | ❌ No    | `false`       | Forces downloading the images even if they already exist. |
+
 
 ## Contributing
 Contributions are welcome! If you encounter issues, have feature suggestions, or want to improve the package, feel free to open an issue or submit a pull request. Please read our Contribution Guidelines for more information.
---
+++ lib/commands/download_images_from_args.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+
+import 'package:args/command_runner.dart';
+import 'package:figma_vars_to_dart/services/logger/logger.dart';
+
+import '../services/services.dart';
+
+class DownloadImagesFromArgsCommand extends Command {
+  final FigmaService figmaApi;
+  final ParserService parser;
+  final WriterService writer;
+  final CodeGeneratorService generator;
+  final LoggerService logger;
+
+  DownloadImagesFromArgsCommand({
+    required this.figmaApi,
+    required this.parser,
+    required this.writer,
+    required this.generator,
+    required this.logger,
+  }) {
+    argParser.addOption(
+      'fileId',
+      mandatory: true,
+      help: 'The id of the figma file, can be found in the URL.',
+    );
+    argParser.addOption(
+      'token',
+      mandatory: true,
+      help: 'Your Figma personal access token.',
+    );
+    argParser.addOption(
+      'outputFolder',
+      mandatory: false,
+      defaultsTo: 'assets',
+      help: 'Folder for the downloaded images, defaults to /assets.',
+    );
+    argParser.addOption(
+      'scale',
+      mandatory: false,
+      defaultsTo: '1.0,2.0,3.0',
+      help:
+          'Specify the scales of images to download (comma-separated), must be a number between 0.01 and 4. Defaults to 1.0,2.0,3.0. Warning: The "scale" argument is ignored for SVG format.',
+    );
+    argParser.addOption('format',
+        mandatory: false,
+        defaultsTo: 'png',
+        allowed: ['jpg', 'png', 'svg'],
+        help: 'Format of the downloaded images, defaults to png.');
+    argParser.addOption('section',
+        mandatory: false,
+        defaultsTo: 'APP_ASSET_',
+        help:
+            r'Specify sections to download (comma-separated), defaults to all APP_ASSET_$section sections.');
+    argParser.addFlag('force',
+        defaultsTo: false,
+        help:
+            'Force download the images even if they already exist, defaults to false.');
+  }
+
+  @override
+  String get description =>
+      'Download images from Figma sections using command line args.';
+
+  @override
+  String get name => 'download-images';
+
+  @override
+  FutureOr? run() async {
+    final args = argResults;
+    if (args == null) {
+      return;
+    }
+
+    final fileId = args['fileId'] as String;
+    final token = args['token'] as String;
+    final outputFolder = args['outputFolder'] as String;
+    final imageFormat = args['format'] as String;
+    final scales = args['scale'] as String;
+    final forceDownload = args['force'] as bool;
+
+    List<String> sectionsToDownload = [];
+    if ((args['section'] as String) != 'APP_ASSET_') {
+      sectionsToDownload =
+          (args['section'] as String).split(',').map((e) => e.trim()).toList();
+    }
+
+    List<double> scalesList = handleScales(imageFormat, scales);
+
+    logger.log('\n📥 Fetching Figma file...');
+    final fileData =
+        await figmaApi.fetchFigmaFile(fileId: fileId, token: token);
+
+    logger.log("📂 Identifying 'APP_ASSET_' sections...");
+    final assets = figmaApi.findAppAssets(fileData, sectionsToDownload);
+
+    if (assets.isEmpty) {
+      logger.log("❌ No 'APP_ASSET_' sections found.");
+      return;
+    }
+
+    for (var sectionName in assets.keys) {
+      logger.log('\n📥 Processing images for section: $sectionName');
+
+      List<Map<String, String>> assetList =
+          assets[sectionName]!; // Full asset list (id + name)
+      List<String> nodeIds =
+          assetList.map((e) => e['id']!).toList(); // Extract only IDs
+
+      // Loop over the scales list, fetch and download images for each scale
+      for (var scale in scalesList) {
+        logger.log('  🔄 Fetching ${scale}x images...');
+
+        final imageUrls = await figmaApi.fetchImageUrls(
+          fileId: fileId,
+          token: token,
+          nodeIds: nodeIds,
+          imageFormat: imageFormat,
+          scale: scale,
+        );
+
+        await figmaApi.downloadImages(
+            parentDirectory: outputFolder,
+            imageUrls: imageUrls,
+            sectionName: sectionName,
+            imageFormat: imageFormat,
+            scale: scale,
+            assets: assetList,
+            forceDownload: forceDownload);
+      }
+    }
+
+    logger.log('\n🎉 All images downloaded successfully!');
+  }
+
+  List<double> handleScales(String format, String scales) {
+    // 'scale' is a comma-separated string
+    final scalesList = scales.split(',').map((e) {
+      final parsedValue = double.tryParse(e.toString().trim());
+      if (parsedValue == null) {
+        throw ArgumentError('Invalid scale value "$e". Must be a number.');
+      }
+      if (parsedValue < 0.01 || parsedValue > 4.0) {
+        throw ArgumentError('Scale value must be between 0.01 and 4.0.');
+      } else {
+        return parsedValue;
+      }
+    }).toList();
+
+    // If format is SVG, enforce scale = 1.0
+    if (format == 'svg') {
+      if (scalesList.length != 1 ||
+          (scalesList.length == 1 && scalesList.first != 1.0)) {
+        logger.log(
+            '\n❗ Warning: The "scale" argument is ignored for SVG format. Using scale = 1.0.');
+      }
+      return [1.0]; // Override with 1.0
+    } else {
+      return scalesList;
+    }
+  }
+}
